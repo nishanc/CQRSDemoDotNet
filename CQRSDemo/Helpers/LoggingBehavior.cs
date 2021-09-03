d475d93d5a1b97be6169e4a667978a06d5188446
Nishan Wickramarathna <nishanw@99x.lk>
2021-09-04 02:37:12 +0530

    Log request data

--- CQRSDemo/Helpers/LoggingBehavior.cs
+++ CQRSDemo/Helpers/LoggingBehavior.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -17,18 +18,27 @@ namespace CQRSDemo.Helpers
 
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-            _logger.LogInformation($"Handling {typeof(TRequest).Name}");
+            _logger.LogInformation($"[START] Handling {typeof(TRequest).Name}");
             var stopwatch = Stopwatch.StartNew();
             TResponse response;
             try
             {
+                try
+                {
+                    var requestData = JsonSerializer.Serialize(request);
+                    _logger.LogInformation($"[DATA] With data: {requestData}");
+                }
+                catch (System.Exception)
+                {
+                    _logger.LogInformation("[Serialization ERROR] Could not serialize the request.");
+                }
                 response = await next();
             }
             finally
             {
                 stopwatch.Stop();
                 _logger.LogInformation(
-                    $"Handled {typeof(TResponse).Name}; Execution time = {stopwatch.ElapsedMilliseconds}ms");
+                    $"[END] Handled {typeof(TResponse).Name}; Execution time = {stopwatch.ElapsedMilliseconds}ms");
             }
             return response;
         }
